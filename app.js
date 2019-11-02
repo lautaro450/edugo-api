ddbc6d0004bfa81a67979cc8f0ed22b6e9cb0223
Dante Perea <lautaro.arg@gmail.com>
2019-11-02 13:45:58 +0800

    jwt-token implemented

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 var express = require("express");
 var app = express();
-
-app.get("/api/create-meeting-rooms", function(req, res) {
-    res.send("OK")
+const jwt = require('jsonwebtoken');
+var user = require('./src/api/user');
+var auth = require('./src/auth.js')
+var meeting_room = require('./src/api/meeting-room');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }))
+// jwt token examples to send in header (Authorization: <jwt-token>)
+app.get("/auth/user", function(req, res) {
+    let token = jwt.sign({
+         "user": {
+             "id": 1,
+             "nickname": "John Smith",
+             "roles": []
+         } 
+        },
+         'secret', { algorithm: 'HS256'});
+    res.send(token);
 });
+app.get("/auth/admin", function(req, res) {
+    let token = jwt.sign({
+        "user": {
+            "id": 1,
+            "nickname": "John Smith",
+            "roles": ['admin']
+        } 
+       },
+        'secret', { algorithm: 'HS256'});
+        res.send(token);
+});
+
+// user
+app.get('/api/users', user.list);
+app.post('/api/users', user.create);
+app.get('/api/meeting-room',auth.isAdmin, meeting_room.list);
+app.get
 
 app.get("/", function(req, res) {
 
---
+++ src/auth.js
@@ -0,0 +1,38 @@
+
+const jwt = require('jsonwebtoken');
+exports.isUser = function(req, res, next) {
+    if (typeof req.headers.authorization !== "undefined") {
+        let token = req.headers.authorization;
+        jwt.verify(token, 'secret', { algorithm: "HS256" }, (err, user) => {   
+            
+            if (err) { 
+                res.status(500).json({ error: "Not Authorized" });
+                throw new Error("Not Authorized");
+            }
+            return next();
+        });
+    } else {
+        res.status(500).json({ error: "Not Authorized" });
+        throw new Error("Not Authorized");
+    }
+}
+exports.isAdmin = function(req, res, next) {
+    if (typeof req.headers.authorization !== "undefined") {
+        let token = req.headers.authorization;
+        jwt.verify(token, 'secret', { algorithm: "HS256" }, (err, user) => {   
+            
+            if (err) { 
+                res.status(500).json({ error: "Not Authorized" });
+                throw new Error("Not Authorized");
+            }
+            if(user.user.roles[0] != 'admin') {
+                res.status(500).json({ error: "Not Authorized" });
+                throw new Error("Not Authorized");
+            }
+            return next();
+        });
+    } else {
+        res.status(500).json({ error: "Not Authorized" });
+        throw new Error("Not Authorized");
+    }
+}
