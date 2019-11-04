b011304ec79fa987fbea766a2efcc3e8859643c1
Dante Perea <lautaro.arg@gmail.com>
2019-11-04 10:27:20 +0800

    adding meeting rooms seeder

---
+++ seeders/20191101231208-seed-meeting-room.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('meeting_rooms', [
+    {
+      name: 'room123',
+      description: 'some description',
+      capacity: 1,
+      createdAt: new Date().toDateString(),
+      updatedAt: new Date().toDateString()
+    }
+  ],{});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('meeting_rooms', null, {});
+  }
+};
